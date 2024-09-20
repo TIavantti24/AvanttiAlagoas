c7af3171d56f3d53befee639ffadbf84e8503d0e
TIavantti24 <progamadortiavantti@gmail.com>
2024-09-20 11:26:54 -0300

    commit finish

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+import pymysql
+
+app = Flask(__name__, static_url_path='/static')
+
+
+
+# Rota para o formulário
+@app.route('/')
+def form():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
